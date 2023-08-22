af56bff14f36d48465287da2064aebbc8612c7a9
ilahlah <141638857+ilahlah@users.noreply.github.com>
2023-08-22 21:24:32 +0100

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='n1ynuzzbb7(ml5x$ggz6tg3)1b8x0t!t9m=7!zb#2($@=c+&&2'
+SECRET_KEY=os.environ.get('SECRET_KEY', default='n1ynuzzbb7(ml5x$ggz6tg3)1b8x0t!t9m=7!zb#2($@=c+&&2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 
 # Application definition
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     # Third party apps.
     'bootstrap4',
     # Default django apps.
+    'render.apps.RenderConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
