229edb972e620952b5127812eb54b6c07d7045aa
ilahlah <141638857+ilahlah@users.noreply.github.com>
2023-08-22 21:48:15 +0100

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     # Third party apps.
     'bootstrap4',
     # Default django apps.
-    'render.apps.RenderConfig'
+    'render.apps.RenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
